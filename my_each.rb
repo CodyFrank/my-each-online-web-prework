bfcc91f7948f4526a2b775bf537c3b4d69bb43a4
Cody Frank <cody.frank30@gmail.com>
2019-03-24 23:20:49 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,9 @@
 def my_each(array)
   if block_given?
-    counter = 0
-    while counter <= array.length
-      yeild
+    index = 0
+    while index <= array.size
+      yeild(array[index])
       counter += 1
     end
   end
 end
-
-my_each()
